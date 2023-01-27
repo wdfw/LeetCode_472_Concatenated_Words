7a7569dc5b2e76f0d8a8b8c2825e2ee8feb05d9b
wdfw <111077328+wdfw@users.noreply.github.com>
2023-01-28 01:08:02 +0800

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -6,24 +6,19 @@ class Solution:
         MiniLen = len(words[0])
         res = []
         def fun(w,index,passed) :
-
-            
+            passed[index] = True
             if index >= len(w) : return True
+            
             for i in range(index+1,len(w) + 1) :
-                if not passed[i-1] : break
-                if w[index:i] in Word : 
+                if w[index:i] in Word and not passed[i]: 
                     if fun(w,i,passed) : return True
-                    else : passed[index-1] = False
-                        
+                    
             if index == 0 :  Word[w] = True
             return False
-        
         
         for w in words :
             if len(w) == MiniLen : Word[w] = True
             else : 
-                a = [True]*len(w)
-                if fun(w,0,a) : res.append(w)
-                print(w,a)
-                
+                if fun(w,0, [False]*(len(w)+1)) : res.append(w)
+                    
         return(res)
